4851c66f65ec05d2badd3fbefca8611fab53aae0
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2024-02-06 15:04:52 +0200

    wip: get username from auth api

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,4 +1,4 @@
-import {MediaItem} from '../types/DBtypes';
+import {MediaItem, MediaItemWithOwner} from '../types/DBtypes';
 import MediaRow from '../components/mediaRow';
 import { useEffect, useState } from 'react';
 import { fetchData } from '../lib/functions';
@@ -9,10 +9,18 @@ const Home = () => {
 
   const getMedia = async () => {
     try {
-      const data = await fetchData<MediaItem[]>('data.json')
+      const data = await fetchData<MediaItem[]>(import.meta.env.VITE_MEDIA_API + '/media')
+
+      const dataWithOwner: MediaItemWithOwner = await Promise.all(data.map((item) => {
+        const username = fetchData(import.meta.VITE_AUTH_API + '/users/' + item.user_id);
+        const itemWithOwner: MediaItemWithOwner = item;
+
+      }));
+
       setMediaArray(data);
+      console.log('mediaArray', data);
     } catch (error) {
-      console.log(error);
+      console.log('getMedia failed', error);
     }
   };
 
