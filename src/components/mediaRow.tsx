d9edcaf8774acd75efa2e10734c71f75d7ab42fe
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2024-02-07 13:30:10 +0200

    lab 2

--- src/components/mediaRow.tsx
+++ src/components/mediaRow.tsx
@@ -1,8 +1,8 @@
 import {Link} from 'react-router-dom';
-import {MediaItem} from '../types/DBtypes';
+import { MediaItemWithOwner} from '../types/DBtypes';
 
 const MediaRow = (props: {
-  item: MediaItem;
+  item: MediaItemWithOwner;
 }) => {
   const {item}  = props;
 
@@ -10,6 +10,9 @@ const MediaRow = (props: {
     <tr className="media-row">
       <td className="user_photo">
         <img className="user_photo"src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>
+        {item.username}
       </td>
       <td>
         <Link to="/single" state={item}>View</Link>
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -2,23 +2,23 @@ import {MediaItem, MediaItemWithOwner} from '../types/DBtypes';
 import MediaRow from '../components/mediaRow';
 import { useEffect, useState } from 'react';
 import { fetchData } from '../lib/functions';
-// import UserInfo from '../components/userInfo';
 
 const Home = () => {
   const [mediaArray, setMediaArray] = useState<MediaItem[]>([]);
 
   const getMedia = async () => {
     try {
+
       const data = await fetchData<MediaItem[]>(import.meta.env.VITE_MEDIA_API + '/media')
 
-      const dataWithOwner: MediaItemWithOwner = await Promise.all(data.map((item) => {
-        const username = fetchData(import.meta.VITE_AUTH_API + '/users/' + item.user_id);
-        const itemWithOwner: MediaItemWithOwner = item;
-
+      const dataWithOwner: MediaItemWithOwner[] = await Promise.all(data.map(async (item) => {
+        const owner = await fetchData(import.meta.env.VITE_AUTH_API + '/users/' + item.user_id);
+        const itemWithOwner: MediaItemWithOwner = {...item, username: owner.username};
+        return itemWithOwner;
       }));
 
       setMediaArray(data);
-      console.log('mediaArray', data);
+      console.log('mediaArray');
     } catch (error) {
       console.log('getMedia failed', error);
     }
@@ -32,14 +32,6 @@ const Home = () => {
   return (
     <>
       <table>
-      {/*  <thead>
-         {mediaArray.map((item) => (
-           <UserInfo
-              key={item.user_id}
-              item={item}
-              />
-         ))}
-         </thead> */}
         <tbody>
           {mediaArray.map((item) => (
             <MediaRow
